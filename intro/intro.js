d794381c2a5956a179d86eec3a63e78cf265effb
Guilherme Ferreira <ferreiraga@outlook.com>
2020-04-21 19:53:53 -0300

    savinf work

---
+++ intro/intro.js
@@ -0,0 +1,26 @@
+
+/*
+    PARTE 1: O Javascript trabalha com tipagem dinamica. Assim, uma variável pode assumir qualquer valor.
+*/
+var umTexto = "Olá, sou um texto"
+var umNumero = 10
+var umaLista = [1, 2, 3]
+
+console.log(umTexto);
+// outuput: "Olá sou um texto"
+
+console.log(umNumero);
+// outuput: 10
+
+console.log(umaLista);
+// outuput: [1, 2, 3]
+console.log(umaLista[0]);
+// outuput: 1
+
+
+var umaFuncaoQueDizOi = function(){
+    console.log("Sou a função que diz Oi !!!")
+}
+
+umaFuncaoQueDizOi();
+// output: Sou a função que diz Oi !!!
